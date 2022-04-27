202312efce71310fa54abc39f462c2457f5cbcbe
Pablo <pablinho12fernandes@gmail.com>
2022-04-27 10:14:22 -0300

    projeto n1

---
+++ projeto01/listar_usuario.php
@@ -0,0 +1,38 @@
+<?php  
+    include('conexao.php');
+    $sql = 'SELECT * FROM usuario';
+    //retorna todos os dados da consulta
+    $result = mysqli_query($con, $sql);
+    //retorna a primeira linha
+    $row = mysqli_fetch_array($result);
+    //prit_r($row);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Listagerm de usuarios</title>
+</head>
+<body>
+    <h1>Listagem de usuarios</h1>
+    <table align="center" border="1" width=500>
+    <tr>
+        <th>Codigo</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Telefone</th>
+    </tr>
+    <?php
+        while($row = mysqli_fetch_array($result))
+        {
+            echo "<tr>";
+            echo "<td>" . $row['id_usuario'] . "</td>";             
+            echo "<td>" . $row['nome_usuario'] . "</td>"; 
+            echo "<td>" . $row['email_usuario'] . "</td>"; 
+            echo "<td>" . $row['telefone_usuario'] . "</td>"; 
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
