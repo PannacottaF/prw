ff974aade07ae0f5857478394213000466515615
Pablo <pablinho12fernandes@gmail.com>
2022-08-10 10:46:04 -0300

    a

--- sql_test.php
+++ sql_test.php
@@ -11,3 +11,28 @@
 
     $entidade = 'usuario';
     $criterio = [['id', '=', $id]];
+    $campos = ['id', 'nome', 'email'];
+    print_r($dados);
+    echo'<br>';
+    print_r($campos);
+    echo'<br>';
+    print_r($criterio);
+    echo'<br>';
+
+    // Teste geração INSERT
+    $instrucao = insert($entidade, $dados);
+    echo $instrucao.'<BR>';
+
+    // Teste geração UPDADE
+    $instrucao = insert($entidade, $dados, $criterio);
+    echo $instrucao.'<BR>';
+
+    // Teste geração SELECT
+    $instrucao = insert($entidade, $campos, $criterio);
+    echo $instrucao.'<BR>';
+
+    // Teste geração DELETE
+    $instrucao = insert($entidade, $criterio);
+    echo $instrucao.'<BR>';
+    ?>
+
