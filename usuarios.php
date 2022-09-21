61e507d5425b3b1e30b56932c9de3182e75922ee
Pablo <pablinho12fernandes@gmail.com>
2022-09-21 09:14:35 -0300

    qeth

--- usuarios.php
+++ usuarios.php
@@ -6,5 +6,23 @@
              href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
     </head>
     <body>
-        <div class
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        include 'includes/topo.php';
+                        include 'includes/valida_login.php';
+                        if ($_SESSION['login']['usuario']['adm'] !==){
+                            header('Location: index.php');
+                        }
+                    ?>
+                </div>
+            </div>
+            <div class="row" style="min-height: 500px;">
+                <div class="col-md-12">
+                    <?php include 'includes/menu.php'; ?>
+                </div>
+                <div class="col-md-10" style="padding-top: 50px
+            <h2>Usuario</h2>
+        </div>
 </html>
